b9c6de54dd8591f97def7d0a17f8f594bc6aca85
yon9woo <sevll5953@naver.com>
2021-10-28 17:19:30 +0900

    1

---
+++ 06_DWG/js/script.js
@@ -0,0 +1,7 @@
+ function over(obj){          // over:함수이름으로 사용자지정/onmouseover와 동일
+	 	// alert("마우스오버금지"); // 경고창
+	obj.src="../../images/banana.png";
+}	
+function out(obj){
+	obj.src="../../images/apple.png"
+}
